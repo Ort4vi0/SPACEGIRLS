cee68219af5646a97527a9ee5640db80d9e6e713
Nicolas15 <nicoantoni15@gmail.com>
2025-07-24 08:54:55 -0300

    vilão do código

--- spacegirls.js
+++ spacegirls.js
@@ -1,1 +1,11 @@
+const { clear } = require("console");
+const rl = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
+function filtrarPrioridade(){
+    console.clear()
+    console.log('Filtre as missões por prioridade\nAs PRIORIDADES vão de 1 a 5, com importância crescente.')
+    rl.question('Digite a prioridade desejada: ')
+}
