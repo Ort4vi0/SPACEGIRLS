6978e5b83ef03f8cc4a5e36520937fe325459dd1
Paraizo <eduardoparize.05@gmail.com>
2025-07-24 10:47:32 -0300

    atualizando o adicionar missao

--- spacegirls.js
+++ spacegirls.js
@@ -52,9 +52,12 @@ function exibirMenu() {
 }
 
 function adicionarMissao() {
+  console.clear();
   rl.question("Insira o nome da missão que deseja adicionar: \n", (nome) => {
+    console.clear()
     rl.question("Insira o destino da missão: ", (destino) => {
       destino = destino.toLowerCase();
+      console.clear()
       rl.question(
         "Insira qual a prioridade da missão:\n1-Minima\n2-Baixa\n3-Média\n4-Alta\n5-Crítico\n",
         (prioridade) => {
@@ -67,19 +70,25 @@ function adicionarMissao() {
 }
 
 function adicionarTripulante(nome, destino, prioridade, tripulantesAtuais) {
+    console.clear()
   rl.question(
     `Insira o nome do tripulante que seja adicionar na missão "${nome}":\n`,
     (nomeTripulante) => {
       tripulantesAtuais.push(nomeTripulante);
+      console.clear();
+      console.log(`Tripulante '${nomeTripulante}' adicionado na missao "${nome}"`);
+      console.log("--------------------------------------")
       rl.question(
         `Deseja adicionar outro tripulante na missão ${nome}?\n1-Sim\nOutro-Não\n`,
         (outroTripulante) => {
           outroTripulante = parseInt(outroTripulante);
           switch (outroTripulante) {
             case 1:
+                console.clear()
               adicionarTripulante(nome, destino, prioridade, tripulantesAtuais);
               break;
             default:
+                
               const missao = {
                 Nome: nome,
                 Destino: destino,
@@ -87,10 +96,9 @@ function adicionarTripulante(nome, destino, prioridade, tripulantesAtuais) {
                 tripulantes: tripulantesAtuais,
                 concluida: false,
               };
-              // Tem que ter um array de tripulante dentro do objeto missao
-              // Adicionar ao ultimo indice do array o tripulante
+              console.clear()
               missoes.push(missao);
-              console.log("Missao adicionada com sucesso!");
+              console.log("Missao adicionada com sucesso!\n");
               exibirMenu();
               break;
           }
