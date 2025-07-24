c0f8a2b50d616851ddf8263a6f704bce3c42e65b
Nicolas15 <nicoantoni15@gmail.com>
2025-07-24 10:50:21 -0300

    vilão do código

--- spacegirls.js
+++ spacegirls.js
@@ -51,6 +51,62 @@ function exibirMenu() {
   });
 }
 
+function adicionarMissao() {
+    rl.question("Insira o nome da missão que deseja adicionar: \n", (nome) => {
+      rl.question("Insira o destino da missão: ", (destino) => {
+        destino = destino.toLowerCase();
+        rl.question(
+          "Insira qual a prioridade da missão:\n1-Minima\n2-Baixa\n3-Média\n4-Alta\n5-Crítico\n",
+          (prioridade) => {
+            prioridade = parseInt(prioridade);
+            adicionarTripulante(nome, destino, prioridade, []);
+          }
+        );
+      });
+    });
+  }
+  
+  function adicionarTripulante(nome, destino, prioridade, tripulantesAtuais) {
+    rl.question(
+      `Insira o nome do tripulante que seja adicionar na missão "${nome}":\n`,
+      (nomeTripulante) => {
+        tripulantesAtuais.push(nomeTripulante);
+        rl.question(
+          `Deseja adicionar outro tripulante na missão ${nome}?\n1-Sim\nOutro-Não\n`,
+          (outroTripulante) => {
+            outroTripulante = parseInt(outroTripulante);
+            switch (outroTripulante) {
+              case 1:
+                adicionarTripulante(nome, destino, prioridade, tripulantesAtuais);
+                break;
+              default:
+                const missao = {
+                  Nome: nome,
+                  Destino: destino,
+                  Prioridade: prioridade,
+                  tripulantes: tripulantesAtuais,
+                  concluida: false,
+                };
+                // Tem que ter um array de tripulante dentro do objeto missao
+                // Adicionar ao ultimo indice do array o tripulante
+                missoes.push(missao);
+                console.log("Missao adicionada com sucesso!");
+                exibirMenu();
+                break;
+            }
+          }
+        );
+      }
+    );
+  }
+  
+  function listarMissoes() {
+    missoes.forEach((index, missao) => {
+      console.log(missoes);
+      exibirMenu();
+    });
+  }
+
 function filtrarPrioridade() {
   console.clear();
   console.log(
@@ -71,3 +127,5 @@ function filtrarPrioridade() {
     }
   });
 }
+
+exibirMenu();
